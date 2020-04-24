be470704017c70d9463f4d99929b9a54a5946a17
Shane Hudson <shane@shanehudson.net>
2020-04-24 15:28:42 +0100

    Allows empty release notes to still be posted

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ from feature_jira import create_jira_release
 logging.basicConfig(level=logging.DEBUG)
 logging.getLogger("slack").setLevel(logging.WARNING)
 logging.getLogger("asyncio").setLevel(logging.WARNING)
+logging.getLogger("urllib3.connectionpool").setLevel(logging.WARNING)
 
 app = Flask(__name__)
 
@@ -175,16 +176,19 @@ def process_interactive():
         )
 
 
+        # If release notes are given, add those to the post. Otherwise still add it as an empty post.
+        release_notes = " "
         if "value" in state_values["release_notes"]["txt_release_notes"]:
             release_notes = state_values["release_notes"]["txt_release_notes"]["value"]
-            file_response = client.files_upload(
-                content = release_notes,
-                filename = f"Change {change_number}",
-                filetype = "post",
-                channels = new_channel_id
-            )
-            file_timestamp = file_response["file"]["shares"]["public"][new_channel_id][0]["ts"]
-            client.pins_add(channel=new_channel_id, timestamp=file_timestamp)
+
+        file_response = client.files_upload(
+            content = release_notes,
+            filename = f"Change {change_number}",
+            filetype = "post",
+            channels = new_channel_id
+        )
+        file_timestamp = file_response["file"]["shares"]["public"][new_channel_id][0]["ts"]
+        client.pins_add(channel=new_channel_id, timestamp=file_timestamp)
         
         return make_response("", 200)
 
