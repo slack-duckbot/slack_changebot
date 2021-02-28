7aab09da3c4c55ebfd3a9a6c91559b9d08f6f697
Matt Stibbs <git@stibbsy.co.uk>
2021-02-28 23:35:16 +0000

    Wrap Jira create version call in try/except

--- app/features/jira.py
+++ app/features/jira.py
@@ -1,6 +1,7 @@
 from datetime import date
+import logging
 
-from jira import JIRA
+from jira import JIRA, exceptions
 
 from app import app
 
@@ -17,13 +18,17 @@ def create_jira_release(change_number, user_name, description):
     if app.config["ENABLE_JIRA_INTEGRATION"]:
         today = date.today()
         change_name = app.config["JIRA_PREFIX"] + change_number
-        version = jira_client.create_version(
-            name=change_name,
-            project=app.config["JIRA_PROJECT_KEY"],
-            startDate=today.strftime("%Y-%m-%d"),
-            description=description,
-        )
-        version_id = version.id
-        return f"{app.config['JIRA_SERVER']}/projects/{app.config['JIRA_PROJECT_KEY']}/versions/{version_id}"
+        try:
+            version = jira_client.create_version(
+                name=change_name,
+                project=app.config["JIRA_PROJECT_KEY"],
+                startDate=today.strftime("%Y-%m-%d"),
+                description=description,
+            )
+            version_id = version.id
+            return f"{app.config['JIRA_SERVER']}/projects/{app.config['JIRA_PROJECT_KEY']}/versions/{version_id}"
+        except exceptions.JIRAError:
+            logging.exception("Got Jira error")
+            return False
     else:
         return False
