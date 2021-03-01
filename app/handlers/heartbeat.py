ff4c2cfb14b13a106544d67d8a5fa2127a47c632
Matt Stibbs <git@stibbsy.co.uk>
2021-03-01 14:46:10 +0000

    Remove the rq job from heartbeat because it was a bit silly

--- app/handlers/heartbeat.py
+++ app/handlers/heartbeat.py
@@ -1,13 +1,6 @@
 from app import app
 
-from app.helpers.redis import redis_q
-
-
-def rq_worker():
-    return True
-
 
 @app.route("/heartbeat", methods=["GET"])
 def heartbeat():
-    redis_q.enqueue(rq_worker)
     return "", 200
