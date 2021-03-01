1c4fd9600112721d786d7415d354be56bf319d80
Matt Stibbs <git@stibbsy.co.uk>
2021-03-01 08:48:23 +0000

    Add a background job to the heartbeat

--- app/handlers/heartbeat.py
+++ app/handlers/heartbeat.py
@@ -1,6 +1,13 @@
 from app import app
 
+from app.helpers.redis import redis_q
+
+
+def rq_worker():
+    return True
+
 
 @app.route("/heartbeat", methods=["GET"])
 def heartbeat():
+    redis_q.enqueue(rq_worker)
     return "", 200
